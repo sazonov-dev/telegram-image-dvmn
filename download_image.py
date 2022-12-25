2ef9e9ddc8aad5a8ff6ab98937aa8f8bc846617b
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2022-12-25 17:57:49 +0300

    three step / download func

--- download_image.py
+++ download_image.py
@@ -1,11 +1,9 @@
 import requests
 
+def download_image(file_url, path):
+    response = requests.get(file_url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
 
-filename = 'images/hubble.jpeg'
-url = "https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg"
-
-response = requests.get(url)
-response.raise_for_status()
-
-with open(filename, 'wb') as file:
-    file.write(response.content)
+download_image("https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg", 'images/hubble.jpeg')
