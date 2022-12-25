41e70cdd205a63f08c70a07be19f14f8453d0fd7
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2022-12-25 19:55:50 +0300

    eight step / get file extension

--- download_image.py
+++ download_image.py
@@ -1,7 +1,16 @@
 import requests
+import os
 
-def download_image(file_url, path):
-    response = requests.get(file_url)
+from urllib.parse import urlparse, unquote
+from dotenv import load_dotenv
+load_dotenv()
+
+
+splitext = os.path.splitext
+nasa_api_token = os.environ["NASA_API_TOKEN"]
+
+def download_image(url, path):
+    response = requests.get(url)
     response.raise_for_status()
     with open(path, 'wb') as file:
         file.write(response.content)
@@ -18,6 +27,19 @@ def fetch_spacex_last_launch():
     for image_number, images in enumerate(images):
         download_image(images, f'images/spacex{image_number}.jpg')
 
+def get_ext(url):
+    response = requests.get(url + nasa_api_token)
+    response.raise_for_status()
+    nasa_info = response.json()
+    url_parse = urlparse(nasa_info['url'])
+    unquote_parse = unquote(url_parse.scheme + url_parse.netloc + url_parse.path)
+    extension = splitext(unquote_parse)[1]
+    return extension
+
+
+
 # download_image("https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg", 'images/hubble.jpeg')
 # get_spacex_images()
-fetch_spacex_last_launch()
+# fetch_spacex_last_launch()
+
+get_ext('https://api.nasa.gov/planetary/apod?api_key=')
