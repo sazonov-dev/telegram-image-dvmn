4871117c44768688a15f8af5278ae8c076370285
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-01-13 15:39:51 +0300

    fix five step

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -9,10 +9,6 @@ def fetch_spacex_image(id):
         response = requests.get(f'https://api.spacexdata.com/v5/launches/{id}')
     response.raise_for_status()
     spacex_response_content = response.json()
-    download_spacex_image(spacex_response_content)
-
-def download_spacex_image(response):
-    images = response['links']['flickr']['original']
+    images = spacex_response_content['links']['flickr']['original']
     for image_number, images in enumerate(images):
         download_img(images, f'images/spacex_{image_number}.jpg')
-
