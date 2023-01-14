5ce0c8ab474731ed26ad127408ab40a4eeb72989
Nikita Sazonov <proger@MacBook-Pro-Nikita.local>
2023-01-14 12:19:10 +0300

    fix first step

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -3,10 +3,9 @@ import requests
 from download_image import download_img
 
 def fetch_spacex_image(id):
+    response = requests.get(f'https://api.spacexdata.com/v5/launches/{id}')
     if not id:
         response = requests.get(f'https://api.spacexdata.com/v5/launches/latest')
-    else:
-        response = requests.get(f'https://api.spacexdata.com/v5/launches/{id}')
     response.raise_for_status()
     spacex_response_content = response.json()
     images = spacex_response_content['links']['flickr']['original']
